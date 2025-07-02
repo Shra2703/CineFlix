0405da509c28b3a71eb5b0e070af00c5154abcbc
shraddha goyal <shraddhagoyal800@gmail.com>
2025-07-02 09:48:33 +0530

    created redux store with user slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
 // components
 import SignInOut from "./components/SignInOut";
 
-function App(){
+// redux store
+import appStore from "./utils/redux/appStore";
 
+function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element:<SignInOut />
+      element: <SignInOut />,
     },
     {
-      path:"/browse",
-      element:<h1>Browse</h1>
-    }
-  ])
-  return(
-   <RouterProvider router={router} />
-  )
+      path: "/browse",
+      element: <h1>Browse</h1>,
+    },
+  ]);
+  return (
+    <Provider store={appStore}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
 export default App;
